6cc153cb3c9ab1b0b59fd34ecc28529c928e32f3
Vladimir Grinenko <i@tadatuta.com>
2018-03-24 19:12:17 +0300

    Support for external config

--- index.js
+++ index.js
@@ -4,19 +4,6 @@ const app = express().enable('trust proxy');
 const MongoClient = require('mongodb').MongoClient;
 
 const config = require('./config');
-const port = config.defaultPort;
-const mongoUrl = config.mongo;
-const urlPrefix = config.pathPrefix;
-
-const dbConnection = new Promise((resolve, reject) => {
-    MongoClient.connect(mongoUrl, function(err, client) {
-        if (err) return reject(err);
-
-        console.log('Connected successfully to mongo');
-
-        resolve(client.db('feedbacks'));
-    });
-});
 
 function find(collection, criteria) {
     return new Promise((resolve, reject) => {
@@ -28,40 +15,57 @@ function find(collection, criteria) {
     });
 }
 
-module.exports = dbConnection.then(db => {
-    const collection = db.collection('docs');
+module.exports = function(opts = {}) {
+    Object.assign(config, opts);
+    const port = config.defaultPort;
+    const mongoUrl = config.mongo;
+    const urlPrefix = config.pathPrefix;
 
-    app
-        .get(`/${urlPrefix}`, (req, res) => {
-            const doc = encodeURIComponent(req.query.doc);
-            find(collection, { doc }).then(docData => {
+    const dbConnection = new Promise((resolve, reject) => {
+        MongoClient.connect(mongoUrl, function(err, client) {
+            if (err) return reject(err);
 
-                const total = docData.reduce((acc, feedback) => {
-                    return acc + +feedback.rating;
-                }, 0);
+            console.log('Connected successfully to mongo');
 
-                res.send({
-                    total,
-                    votes: docData.length
-                });
-            });
-        })
-        .post(`/${urlPrefix}`, (req, res) => {
-            const data = Object.assign({
-                ip: req.headers['x-forwarded-for'] || req.connection.remoteAddress,
-                date: new Date(),
-            }, req.query);
-
-            collection.insert(data, (err, result) => {
-                if (err) return res.status(500).send('DB error :(');
-
-                res.send('ok');
-            });
+            resolve(client.db('feedbacks'));
         });
-
-    module.parent || app.listen(port, () => {
-        console.log('Server is listening on', port);
     });
 
-    return app;
-});
+    return dbConnection.then(db => {
+        const collection = db.collection('docs');
+
+        app
+            .get(`/${urlPrefix}`, (req, res) => {
+                const doc = encodeURIComponent(req.query.doc);
+                find(collection, { doc }).then(docData => {
+
+                    const total = docData.reduce((acc, feedback) => {
+                        return acc + +feedback.rating;
+                    }, 0);
+
+                    res.send({
+                        total,
+                        votes: docData.length
+                    });
+                });
+            })
+            .post(`/${urlPrefix}`, (req, res) => {
+                const data = Object.assign({
+                    ip: req.headers['x-forwarded-for'] || req.connection.remoteAddress,
+                    date: new Date(),
+                }, req.query);
+
+                collection.insert(data, (err, result) => {
+                    if (err) return res.status(500).send('DB error :(');
+
+                    res.send('ok');
+                });
+            });
+
+        module.parent || app.listen(port, () => {
+            console.log('Server is listening on', port);
+        });
+
+        return app;
+    });
+};
